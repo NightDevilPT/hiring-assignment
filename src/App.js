ed15fe161d7ddecc9ed9858a555bc6aed6acbc38
NightDevilPT <pawankumartadagsingh@gmail.com>
2023-03-11 17:23:54 +0530

    Adding1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import Skeleton from '@mui/material/Skeleton';
+import Alert from '@mui/material/Alert';
+import axios from 'axios';
+
+
 
 function App() {
+  // variable for storing data
+  const [titles,setTitles]=useState(null);
+
+  // fetching data from api only one time
+  useEffect(()=>{
+    axios.get("https://dummyjson.com/posts").then(response=>{
+      setTitles(response.data.posts)
+    }).catch(err=>{
+      setTitles(false);
+    })
+  },[])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {/* 
+      applying condition 
+      if data not fetched then showing skeleton
+      if error occurs then showing error alerts 
+      if data fetched then showing Titles
+    */}
+      {
+        titles===null?
+        <Skeleton className={`loading-skeleton`} animation={"wave"} variant="rounded" width={"90%"} height={50} />:
+        titles===false?
+        <Alert className={`error-alert`} severity="error">This is an Server or API error — check it out!</Alert>:
+        titles.map((items,index)=>{
+          return(
+          <div className='titles' key={items.id}>
+            {items.title}
+          </div>)
+        })
+      }
     </div>
   );
 }
